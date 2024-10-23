510064ab8d9161087d02c8c728bc9fed60b70117
Teresa <chua0033@student.monash.edu>
2024-10-23 13:06:23 +1100

    footer

--- booking-system-frontend/src/components/Footer.tsx
+++ booking-system-frontend/src/components/Footer.tsx
@@ -14,6 +14,7 @@ import EmailIcon from "@mui/icons-material/Email";
 import CopyRight from "./CopyRight";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -30,14 +31,17 @@ function Footer() {
         <Container maxWidth="xl">
           <Stack
             direction="row"
-            divider={<Divider orientation="vertical" flexItem />}
+            gap={3}
             spacing={1}
             useFlexGap
+            sx={{ flexWrap: "wrap" }}
           >
             <Box
               sx={{
                 display: { xs: "flex", md: "flex" },
                 mr: "64px",
+                mt: "64px",
+                mb: "15px",
                 color: "inherit",
               }}
             >
@@ -52,10 +56,17 @@ function Footer() {
                 sx={{ display: { xs: "flex", md: "flex" }, mr: 1 }}
               />
             </Box>
+            <Divider
+              orientation="vertical"
+              flexItem
+              sx={{ mt: "64px", mb: "15px", height: "75px" }}
+            />
             <Box
               sx={{
                 display: { xs: "flex", md: "flex" },
                 ml: "64px",
+                mt: "64px",
+                mb: "15px",
               }}
             >
               <Stack direction="row" spacing={2} useFlexGap>
@@ -126,6 +137,169 @@ function Footer() {
                         info@test.com
                       </Typography>
                     </Typography>
+                  </Stack>
+                </Box>
+              </Stack>
+            </Box>
+            <Box sx={{ mb: "50px" }}>
+              <Stack direction="row" spacing={3} useFlexGap>
+                <Box
+                  sx={{
+                    maxWidth: 260,
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      width: 260,
+                      fontSize: "16px",
+                      fontWeight: "600",
+                      mb: "15px",
+                    }}
+                  >
+                    About Us
+                  </Typography>
+                  <Typography sx={{ fontSize: "16px", fontWeight: "400" }}>
+                    Physio is a premium WordPress theme designed & built for
+                    physical therapy and rehabilitation clinics. The theme has a
+                    drag and drop page builder, is fully customizable and
+                    responsive. Only available on ThemeForest.
+                  </Typography>
+                </Box>
+                <Box sx={{ maxWidth: 260, ml: "50px" }}>
+                  <Typography
+                    sx={{
+                      width: 260,
+                      fontSize: "16px",
+                      fontWeight: "600",
+                      mb: "15px",
+                    }}
+                  >
+                    Extra Navigation
+                  </Typography>
+                  <Stack gap={1} spacing={2} useFlexGap>
+                    <Link to="/">Nav 1</Link> <br />
+                    <Link to="/">Nav 2</Link> <br />
+                    <Link to="/">Nav 3</Link>
+                  </Stack>
+                </Box>
+                <Box sx={{ maxWidth: 260, ml: "50px" }}>
+                  <Typography
+                    sx={{
+                      width: 260,
+                      fontSize: "16px",
+                      fontWeight: "600",
+                      mb: "15px",
+                    }}
+                  >
+                    Our Services
+                  </Typography>
+                  <Stack gap={1} spacing={2} useFlexGap>
+                    <Link to="/">Sevice 1</Link> <br />
+                    <Link to="/">Sevice 2</Link> <br />
+                    <Link to="/">Sevice 3</Link>
+                  </Stack>
+                </Box>
+                <Box sx={{ maxWidth: 260, ml: "50px" }}>
+                  <Typography
+                    sx={{
+                      width: 260,
+                      fontSize: "16px",
+                      fontWeight: "600",
+                      mb: "15px",
+                    }}
+                  >
+                    Opening Hours
+                  </Typography>
+                  <Stack
+                    gap={1}
+                    divider={<Divider orientation="horizontal" flexItem />}
+                    spacing={6}
+                    useFlexGap
+                  >
+                    <Stack spacing={1} direction="row" useFlexGap>
+                      <Typography>Monday</Typography>
+                      <Typography
+                        sx={{
+                          flexGrow: 1,
+                          display: { xs: "none", md: "flex" },
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        07:00 - 17:00
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={1} direction="row" useFlexGap>
+                      <Typography>Tuesday</Typography>
+                      <Typography
+                        sx={{
+                          flexGrow: 1,
+                          display: { xs: "none", md: "flex" },
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        07:00 - 17:00
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={1} direction="row" useFlexGap>
+                      <Typography>Wednesday</Typography>
+                      <Typography
+                        sx={{
+                          flexGrow: 1,
+                          display: { xs: "none", md: "flex" },
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        07:00 - 17:00
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={1} direction="row" useFlexGap>
+                      <Typography>Thursday</Typography>
+                      <Typography
+                        sx={{
+                          flexGrow: 1,
+                          display: { xs: "none", md: "flex" },
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        07:00 - 17:00
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={1} direction="row" useFlexGap>
+                      <Typography>Friday</Typography>
+                      <Typography
+                        sx={{
+                          flexGrow: 1,
+                          display: { xs: "none", md: "flex" },
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        07:00 - 17:00
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={1} direction="row" useFlexGap>
+                      <Typography>Saturday</Typography>
+                      <Typography
+                        sx={{
+                          flexGrow: 1,
+                          display: { xs: "none", md: "flex" },
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        07:00 - 17:00
+                      </Typography>
+                    </Stack>
+                    <Stack spacing={1} direction="row" useFlexGap>
+                      <Typography>Sunday</Typography>
+                      <Typography
+                        sx={{
+                          flexGrow: 1,
+                          display: { xs: "none", md: "flex" },
+                          justifyContent: "flex-end",
+                        }}
+                      >
+                        07:00 - 17:00
+                      </Typography>
+                    </Stack>
                   </Stack>
                 </Box>
               </Stack>
