b24694b6428ea8be3396e56428824eaa736bacb8
Teresa <chua0033@student.monash.edu>
2024-11-03 19:51:31 +1100

    clean up

--- booking-system-frontend/src/components/Header.tsx
+++ booking-system-frontend/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
@@ -16,25 +15,29 @@ function Header() {
         color: "primary.light",
         alignItems: "center",
         justifyContent: "center",
+        width: "100%",
       }}
     >
       <Box
         sx={{
           display: {
             xs: "none",
-            sm: "none",
-            md: "none",
-            lg: "none",
+            sm: "flex",
+            md: "flex",
+            lg: "flex",
             xl: "flex",
           },
           alignItems: "center",
           justifyContent: "center",
+          width: "100%",
         }}
       >
         <Box
           style={{
             position: "relative" /* set container to a relative position */,
             justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
           }}
         >
           <img
@@ -42,7 +45,7 @@ function Header() {
               objectFit: "cover",
               objectPosition: "center center",
               opacity: "50%",
-              minWidth: "1440px",
+              width: "inherit",
               maxHeight: "450px",
             }}
             src="/header.jpg"
@@ -51,10 +54,18 @@ function Header() {
             sx={{
               position: "absolute",
               top: "25%",
-              left: "21%",
+              left: "25%",
+              right: "25%",
               zIndex: "2",
               color: "primary.dark",
               fontSize: "32px",
+              display: {
+                xs: "none",
+                sm: "none",
+                md: "flex",
+                lg: "flex",
+                xl: "flex",
+              },
             }}
           >
             Test Test Test Test Test Test Test Test Test Test Test Test
@@ -65,154 +76,10 @@ function Header() {
             sx={{
               backgroundColor: "primary.dark",
               position: "absolute",
-              top: "50%",
+              top: "60%",
               left: "44%",
               zIndex: "2",
             }}
-          >
-            Book Appointment
-          </Button>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: {
-            xs: "none",
-            sm: "none",
-            md: "none",
-            lg: "flex",
-            xl: "none",
-          },
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          style={{
-            position: "relative" /* set container to a relative position */,
-            justifyContent: "center",
-          }}
-        >
-          <img
-            style={{
-              objectFit: "cover",
-              objectPosition: "center center",
-              opacity: "50%",
-              minWidth: "1180px",
-              maxHeight: "375px",
-            }}
-            src="/header.jpg"
-          ></img>
-          <Typography
-            sx={{
-              position: "absolute",
-              top: "25%",
-              left: "20%",
-              zIndex: "2",
-              color: "primary.dark",
-              fontSize: "32px",
-            }}
-          >
-            Test Test Test Test Test Test Test Test Test Test Test Test
-          </Typography>
-          <Button
-            sx={{
-              backgroundColor: "primary.dark",
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              zIndex: "2",
-            }}
-            component={Link}
-            to="/booking"
-          >
-            Book Appointment
-          </Button>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: { xs: "none", sm: "none", md: "flex", lg: "none" },
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          style={{
-            position: "relative" /* set container to a relative position */,
-            justifyContent: "center",
-          }}
-        >
-          <img
-            style={{
-              objectFit: "cover",
-              objectPosition: "center center",
-              opacity: "50%",
-              minWidth: "880px",
-              maxHeight: "340px",
-            }}
-            src="/header.jpg"
-          ></img>
-          <Typography
-            sx={{
-              position: "absolute",
-              top: "25%",
-              left: "2.3%",
-              zIndex: "2",
-              color: "primary.dark",
-              fontSize: "32px",
-            }}
-          >
-            Test Test Test Test Test Test Test Test Test Test Test Test
-          </Typography>
-          <Button
-            sx={{
-              backgroundColor: "primary.dark",
-              position: "absolute",
-              top: "50%",
-              left: "40%",
-              zIndex: "2",
-            }}
-            component={Link}
-            to="/booking"
-          >
-            Book Appointment
-          </Button>
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: { xs: "none", sm: "flex", md: "none", lg: "none" },
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          style={{
-            position: "relative" /* set container to a relative position */,
-            justifyContent: "center",
-          }}
-        >
-          <img
-            style={{
-              objectFit: "cover",
-              objectPosition: "center center",
-              opacity: "50%",
-              minWidth: "580px",
-              maxHeight: "155px",
-            }}
-            src="/header.jpg"
-          ></img>
-          <Button
-            sx={{
-              backgroundColor: "primary.dark",
-              position: "absolute",
-              top: "50%",
-              left: "35%",
-              zIndex: "2",
-            }}
-            component={Link}
-            to="/booking"
           >
             Book Appointment
           </Button>
--- booking-system-frontend/src/pages/Booking.tsx
+++ booking-system-frontend/src/pages/Booking.tsx
@@ -15,7 +15,6 @@ const Booking: React.FC = () => {
         <CssBaseline />
         <NavigationBar />
         <Container maxWidth="xl">
-          <Typography>Booking Page</Typography>
           <BookingForm />
         </Container>
         <Footer />
