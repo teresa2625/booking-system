0e80fefc6f2b12ef09b16c58c38d220ea0e58a86
Teresa <chua0033@student.monash.edu>
2024-11-10 20:35:24 +1100

    clean up

--- booking-system-frontend/src/components/BookingForm.tsx
+++ booking-system-frontend/src/components/BookingForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Booking } from "../types/booking";
 import dayjs, { Dayjs } from "dayjs";
-import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker";
