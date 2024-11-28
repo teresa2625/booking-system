ae792df0b030f330783118c6cbfa09e74b5cdb53
Teresa <chua0033@student.monash.edu>
2024-11-28 22:17:44 +1100

    combine

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,35 +100,6 @@ resource "aws_cloudfront_distribution" "bsf_distribution" {
 
   restrictions {
     geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
-
-
-
-# CloudFront Distribution
-resource "aws_cloudfront_distribution" "bsf_distribution" {
-  enabled = true
-
-  origin {
-    domain_name = aws_s3_bucket.bsf_bucket.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.frontend_bucket.id}"
-  }
-
-  default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "S3-${aws_s3_bucket.bsf_bucket.id}"
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  restrictions {
-    geo_restriction {
       restriction_type = "none" # Allow access from all countries
     }
   }
